a858c86f8096c78a054c65021d0da6dda8778b4d
unknown <limah@coventry.ac.uk>
2021-10-28 20:44:34 +0100

    end gamet tha

---
+++ Construction/Assets/Scripts/elevator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator : MonoBehaviour
+{
+
+    public GameObject elevatorDoor;
+    public GameObject endGame;
+    bool activated;
+
+    public void Activate()
+    {
+        activated = true;
+    }
+    private void Update()
+    {
+
+        {
+            if (activated)
+            {
+                elevatorDoor.gameObject.transform.localScale = new Vector3(elevatorDoor.gameObject.transform.localScale.x, elevatorDoor.gameObject.transform.localScale.y, elevatorDoor.gameObject.transform.localScale.z - .5f);
+
+                if (elevatorDoor.gameObject.transform.localScale.z <= .3f && !endGame.activeSelf)
+                {
+                    endGame.SetActive(true);
+                }
+            }
+        }
+    }
+}
